7c03ab650c1ba2bfa2f4ed047cf582d3d706e662
Liz Johnson <liz.jade.johnson@gmail.com>
2024-04-07 17:08:10 -0400

    finished with template examples and choice button

--- getFilms.mjs
+++ getFilms.mjs
@@ -1,13 +1,13 @@
-const options = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNGEzOTliYjU1YTU4NDU1MzlmODZiYjdjNjc1ZjA4ZSIsInN1YiI6IjU5MWVmMGU0YzNhMzY4Nzc0ZTAzNTBjZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2iT8D8OojvpqUbPjhmOXPYd-qRYdQNMipqJ24ZgAGJA",
-  },
-};
+// const options = {
+//   method: "GET",
+//   headers: {
+//     accept: "application/json",
+//     Authorization:
+//       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNGEzOTliYjU1YTU4NDU1MzlmODZiYjdjNjc1ZjA4ZSIsInN1YiI6IjU5MWVmMGU0YzNhMzY4Nzc0ZTAzNTBjZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2iT8D8OojvpqUbPjhmOXPYd-qRYdQNMipqJ24ZgAGJA",
+//   },
+// };
 
-fetch("https://api.themoviedb.org/3/authentication", options)
-  .then((response) => response.json())
-  .then((response) => console.log(response))
-  .catch((err) => console.error(err));
+// fetch("https://api.themoviedb.org/3/authentication", options)
+//   .then((response) => response.json())
+//   .then((response) => console.log(response))
+//   .catch((err) => console.error(err));
--- main.mjs
+++ main.mjs
@@ -1,14 +1,42 @@
+//---------------Main App functions etc---------------
+
 // START BUTTON EVENT LISTENER:
 
 document.addEventListener("DOMContentLoaded", (event) => {
   document.querySelector(".startBtn").addEventListener("click", function () {
-    const options = "Love, Hate, Sadness, Joy, Fun, Confusion, Paranoia";
-    let userMood = prompt("What are you in the mood for? Options: " + options);
-    if (userMood) {
-      userMood = userMood.toUpperCase(); // Make answer uppercase
+    const options = [
+      "love",
+      "hate",
+      "sadness",
+      "happiness",
+      "fun",
+      "confused",
+      "paranoid",
+      "trapped",
+      "lucky",
+    ];
+    let userMood = prompt(
+      "What are you in the mood for? Options: " + options.join(", ")
+    );
+    // Check if the user input is not null and is one of the options
+    if (userMood && options.includes(userMood.trim().toLowerCase())) {
+      userMood = userMood.toUpperCase(); // Make answer uppercase for display
       document.querySelector(
         ".siteTitle"
       ).innerText = `IN THE MOOD FOR ${userMood}`;
+    } else {
+      // Inform the user their choice was not in the list
+      alert("Please choose from the list: " + options.join(", "));
     }
   });
 });
+
+// ---------------Import Modules---------------
+
+// import getFilms from "./getFilms.mjs";
+
+// console.log(getFilms(userMood));
+
+// ---------------[Initialized Functions()]---------------
+
+// getFilms(userMood);
